621ebe78186d9ca22c4c7a439a607fac46a60229
hs.lee0130 <chickenman10@naver.com>
2024-11-23 14:13:03 +0900

    [Feat/1] - CRLF 변경으로 인한 커밋 - gitignore에 테스트 파일 추가 - UserAccountRepository에 대한 테스트 코드 추가

--- src/test/java/manage/store/user/repository/UserAccountRepositoryTest.java
+++ src/test/java/manage/store/user/repository/UserAccountRepositoryTest.java
@@ -5,13 +5,16 @@ import manage.store.user.util.UserUtils;
 import manage.store.user.servlet.UserApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 @MybatisTest
 @ContextConfiguration(classes = UserApplication.class)
@@ -35,6 +38,37 @@ class UserAccountRepositoryTest {
         }
     }
 
+    /** select */
+    @Test
+    @Order(1)
+    @DisplayName("select 성공")
+    void selectUserById_success() {
+        // given
+        User user = users[0];
+
+        // when
+        User dbUser = userAccountRepository.selectUserById(user.getId());
+
+        // then
+        assertTrue(UserUtils.compareUser(user, dbUser));
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("select 실패")
+    void selectUserById_fail_noUser() {
+        // given
+        String noUserId = "noUserId";
+
+        // when
+        User dbUser = userAccountRepository.selectUserById(noUserId);
+
+        // then
+        assertThat(dbUser).isNull();
+    }
+
+
+    /** insert */
     @Test
     @DisplayName("insertUser 성공")
     public void insertUser_success() {
@@ -46,77 +80,51 @@ class UserAccountRepositoryTest {
 
         // then
         assertEquals(1, result);
-
-        User dbUser = userAccountRepository.selectUserById(user.getId());
-        assertTrue(UserUtils.compareUser(user, dbUser));
     }
 
     @Test
-    @DisplayName("insertUser 실패 - 중복된 id")
-    public void insertUser_fail_duplicateId() {
+    @DisplayName("insertUser 실패 - 중복된 id(PK)")
+    public void insertUser_fail_duplicateId_PK() {
         // given
         User user = UserUtils.createUser("userId1");
 
-        // when
-        int result = userAccountRepository.insertUser(user);
-
-        // then
-        assertEquals(0, result);
+        // when - then
+        assertThrows(DuplicateKeyException.class, () -> userAccountRepository.insertUser(user));
     }
 
     @Test
-    @DisplayName("insertUser 실패 - 중복된 email")
-    public void insertUser_fail_duplicateEmail() {
+    @DisplayName("insertUser 실패 - 중복된 email(UNIQUE)")
+    public void insertUser_fail_duplicateEmail_UNIQUE() {
         // given
         User user = UserUtils.createUser("userId4");
         user.setEmail(users[0].getEmail());
 
-        // when
-        int result = userAccountRepository.insertUser(user);
-
-        // then
-        assertEquals(0, result);
+        // when - then
+        assertThrows(DuplicateKeyException.class, () -> userAccountRepository.insertUser(user));
+        assertThat(userAccountRepository.selectUserById(user.getId())).isNull();
     }
 
     @Test
-    @DisplayName("insertUser 실패 - 중복된 phoneNo")
-    public void insertUser_fail_duplicatePhoneNo() {
+    @DisplayName("insertUser 실패 - 중복된 phoneNo(UNIQUE)")
+    public void insertUser_fail_duplicatePhoneNo_UNIQUE() {
         // given
         User user = UserUtils.createUser("userId4");
         user.setPhoneNo(users[0].getPhoneNo());
 
-        // when
-        int result = userAccountRepository.insertUser(user);
-
-        // then
-        assertEquals(0, result);
+        // when - then
+        assertThrows(DuplicateKeyException.class, () -> userAccountRepository.insertUser(user));
+        assertThat(userAccountRepository.selectUserById(user.getId())).isNull();
     }
 
     @Test
-    @DisplayName("insertUser 실패 - 중복된 residentRegistNo")
-    public void insertUser_fail_duplicateResidentRegistNo() {
+    @DisplayName("insertUser 실패 - 중복된 residentRegistNo(UNIQUE)")
+    public void insertUser_fail_duplicateResidentRegistNo_UNIQUE() {
         // given
         User user = UserUtils.createUser("userId4");
         user.setResidentRegistNo(users[0].getResidentRegistNo());
 
-        // when
-        int result = userAccountRepository.insertUser(user);
-
-        // then
-        assertEquals(0, result);
-    }
-
-
-
-    @Test
-    void selectUserById() {
-        // given
-        User user = users[0];
-
-        // when
-        User dbUser = userAccountRepository.selectUserById(user.getId());
-
-        // then
-        assertTrue(UserUtils.compareUser(user, dbUser));
+        // when - then
+        assertThrows(DuplicateKeyException.class, () -> userAccountRepository.insertUser(user));
+        assertThat(userAccountRepository.selectUserById(user.getId())).isNull();
     }
 }
